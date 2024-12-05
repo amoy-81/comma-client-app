d5982dff1187bcd031693f63aac45403c5a19300
amoy-81 <ali.moaydi2020@gmail.com>
2024-12-06 00:07:17 +0330

    Navigate to profile after click

--- src/layout/main-layout/sections/connections-panel/components/profile-card/ProfileCard.tsx
+++ src/layout/main-layout/sections/connections-panel/components/profile-card/ProfileCard.tsx
@@ -1,10 +1,21 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import useAuth from "../../../../../../hooks/use-auth.hook";
+import { useNavigate } from "react-router-dom";
 
 const ProfileCard = () => {
+  const navigate = useNavigate();
+
   const { user } = useAuth();
+
+  const handleNavigateProfile = () => {
+    navigate("/profile");
+  };
+
   return (
-    <Box className="w-full h-16 bg-secondary-900 border border-solid border-secondary-800 rounded-3xl flex gap-2 items-center p-2">
+    <Box
+      onClick={handleNavigateProfile}
+      className="w-full h-16 bg-secondary-900 border border-solid border-secondary-800 rounded-3xl flex gap-2 items-center p-2 cursor-pointer"
+    >
       <Avatar
         src={user?.avatar}
         className="!size-11 border-4 border-secondary-900 !text-primary-600 !bg-secondary-600"
