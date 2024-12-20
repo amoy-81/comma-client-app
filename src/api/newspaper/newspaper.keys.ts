93d3bf2f119a41876d36484154c9a2c635ce67a4
amoy-81 <ali.moaydi2020@gmail.com>
2024-12-20 16:25:39 +0330

    add newspaper api querys

--- src/api/newspaper/newspaper.keys.ts
+++ src/api/newspaper/newspaper.keys.ts
@@ -2,4 +2,8 @@ export const NewspaperKeys = {
   create: "create-newspaper",
   getOne: "get-one-newspaper",
   getAll: "get-all-newspaper",
+  addSection: "add-section-newspaper",
+  editSection: "edit-section-newspaper",
+  editInfo: "edit-info-newspaper",
+  deleteSection: "delete-section-newspaper",
 };
