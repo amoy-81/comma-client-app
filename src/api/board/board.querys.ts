f66548943d5ab0506664fc946c0cbb57aa4ca38c
amoy <ali.moaydi2020@gmail.com>
2024-12-27 13:45:25 +0330

    add invalidateQueries for querys

--- src/api/board/board.querys.ts
+++ src/api/board/board.querys.ts
@@ -1,14 +1,18 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { BoardKeys } from "./board.keys";
 import useBoardAction from "./board.actions";
 import { AddBoardRequest, GetBoardParams } from "./board.type";
 
 export const useBoardComment = () => {
+  const queryClient = useQueryClient();
   const { addBoardAction } = useBoardAction();
 
   const { mutate, isPending, isSuccess, data } = useMutation({
     mutationKey: [BoardKeys.add],
     mutationFn: (data: AddBoardRequest) => addBoardAction(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [BoardKeys.get] });
+    },
   });
 
   return {
@@ -19,21 +23,20 @@ export const useBoardComment = () => {
   };
 };
 
-  export const useGetBoard = (params: GetBoardParams) => {
-    const { getBoardAction } = useBoardAction();
-  
-    const { data, isLoading, refetch } = useQuery({
-      queryKey: [BoardKeys.get, params.page, params.pageSize],
-      queryFn: () => getBoardAction(params),
-      staleTime: 0,
-      gcTime: 0,
-      refetchInterval: 20000,
-    });
-  
-    return {
-      getBoardData: data,
-      getBoardLoading: isLoading,
-      refetch,
-    };
+export const useGetBoard = (params: GetBoardParams) => {
+  const { getBoardAction } = useBoardAction();
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: [BoardKeys.get, params.page, params.pageSize],
+    queryFn: () => getBoardAction(params),
+    staleTime: 0,
+    gcTime: 0,
+    refetchInterval: 20000,
+  });
+
+  return {
+    getBoardData: data,
+    getBoardLoading: isLoading,
+    refetch,
   };
-  
+};
