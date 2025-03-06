8029c10fbd583eec1e4396fb2f6954bc1bd02fa1
ashkan <ashkanmofidi16@gmail.com>
2025-03-06 21:53:40 +0330

    fix all bug

--- src/pages/notif/NotifSkeleton.tsx
+++ src/pages/notif/NotifSkeleton.tsx
@@ -6,26 +6,40 @@ const NotifSkeleton = () => {
       {[...Array(5)].map((_, index) => (
         <Box
           key={index}
-          className="relative p-4 bg-secondary-900 rounded-xl grid grid-cols-12 items-center"
+          className="relative p-4 bg-secondary-900 rounded-xl grid grid-cols-12 gap-4 items-center"
           style={{
             border: "1px solid #1E293B",
             boxShadow: "0 4px 6px rgba(0, 0, 0, 0.5)",
-        
           }}
         >
-          <Box className="col-span-1">
-            <Skeleton variant="circular" className="!w-12 !h-12 !bg-secondary-600" /> 
+          <Box className="col-span-2 md:col-span-1">
+            <Skeleton
+              variant="circular"
+              className="!w-12 !h-12 !bg-secondary-600"
+            />
           </Box>
 
-          <Box className="col-span-9 ml-4">
-            <Skeleton variant="text" className="!w-36 h-6 !bg-secondary-600" /> 
-            <Skeleton variant="text" className="!w-28 h-5 !bg-secondary-600 mt-2" /> 
-            <Skeleton variant="text" className="!w-1/2 h-5 !bg-secondary-600 mt-3" /> 
+          <Box className="col-span-8 md:col-span-9 ml-4">
+            <Skeleton variant="text" className="!w-36 h-6 !bg-secondary-600" />
+            <Skeleton
+              variant="text"
+              className="!w-28 h-5 !bg-secondary-600 mt-2"
+            />
+            <Skeleton
+              variant="text"
+              className="!w-1/2 h-5 !bg-secondary-600 mt-3"
+            />
           </Box>
 
           <Box className="col-span-2 flex flex-col items-end">
-            <Skeleton variant="text" className="!w-24 !h-3 !bg-secondary-600 mb-2" /> 
-            <Skeleton variant="rectangular" className="!w-24 h-8 !bg-secondary-600 rounded-sm" /> 
+            <Skeleton
+              variant="text"
+              className="!w-24 !h-3 !bg-secondary-600 mb-2"
+            />
+            <Skeleton
+              variant="rectangular"
+              className="!w-24 h-8 !bg-secondary-600 rounded-sm"
+            />
           </Box>
         </Box>
       ))}
--- src/pages/search/sections/search-posts/SearchSkeleton.tsx
+++ src/pages/search/sections/search-posts/SearchSkeleton.tsx
@@ -3,13 +3,31 @@ import { Box, Skeleton } from "@mui/material";
 export const TopPostSkeleton = () => {
   return (
     <Box className="top-post-skeleton rounded-xl p-6 shadow-lg relative overflow-hidden col-span-1 row-span-2">
-      <Skeleton variant="rectangular" className="absolute inset-0 opacity-25 rounded-xl !h-full !w-full" />
+      <Skeleton
+        variant="rectangular"
+        className="absolute inset-0 opacity-25 rounded-xl !h-full !w-full"
+      />
       <Box className="relative flex flex-col items-center text-center h-full justify-center">
-        <Skeleton variant="circular" className="w-12 h-12 !bg-secondary-600" />
-        <Skeleton variant="text" className="!w-1/2 h-6 mt-4 !bg-secondary-600" />
-        <Skeleton variant="text" className="!w-1/3 h-4 mt-2 !bg-secondary-600" />
-        <Skeleton variant="text" className="!w-1/4 h-4 mt-4 !bg-secondary-600" />
-        <Skeleton variant="rectangular" className="!w-full h-16 mt-6 !bg-secondary-600 rounded-md" />
+        <Skeleton
+          variant="circular"
+          className="!bg-secondary-600 !w-12 !h-12"
+        />
+        <Skeleton
+          variant="text"
+          className="!w-1/2 h-6 mt-4 !bg-secondary-600"
+        />
+        <Skeleton
+          variant="text"
+          className="!w-1/3 h-4 mt-2 !bg-secondary-600"
+        />
+        <Skeleton
+          variant="text"
+          className="!w-1/4 h-4 mt-4 !bg-secondary-600"
+        />
+        <Skeleton
+          variant="rectangular"
+          className="!w-full h-16 mt-6 !bg-secondary-600 rounded-md"
+        />
       </Box>
     </Box>
   );
@@ -17,14 +35,37 @@ export const TopPostSkeleton = () => {
 
 export const PostCardSkeleton = () => {
   return (
-    <Box className="post-card-skeleton rounded-xl p-4 shadow-lg relative overflow-hidden w-80/2 h-36">
-      <Skeleton variant="rectangular" className="absolute inset-0 opacity-20 rounded-xl !h-full !w-full" />
-      <Box className="relative flex items-start space-x-4">
-        <Skeleton variant="circular" className="w-10 h-10 !bg-secondary-600" />
+    <Box className="post-card-skeleton bg-secondary-900 rounded-xl p-4 shadow-lg relative overflow-hidden w-full md:w-80/2 h-36 cursor-pointer">
+      <Skeleton
+        variant="rectangular"
+        className="absolute inset-0 opacity-20 rounded-xl !h-full !w-full"
+      />
+
+      <Box className="hidden md:flex items-start space-x-4 h-full">
+        <Skeleton variant="circular" className="!bg-secondary-600 !w-11 !h-9" />
+
         <Box className="flex flex-col space-y-1 w-full">
           <Skeleton variant="text" className="!w-3/4 h-4 !bg-secondary-600" />
+
           <Skeleton variant="text" className="!w-1/2 h-3 !bg-secondary-600" />
         </Box>
+      </Box>
+
+      <Box className="md:hidden flex flex-col items-center h-full justify-center">
+        <Skeleton
+          variant="circular"
+          className="!bg-secondary-600 !w-12 !h-12"
+        />
+
+        <Skeleton
+          variant="text"
+          className="!w-1/2 h-4 mt-2 !bg-secondary-600"
+        />
+
+        <Skeleton
+          variant="text"
+          className="!w-1/3 h-3 mt-2 !bg-secondary-600"
+        />
       </Box>
     </Box>
   );
